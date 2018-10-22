8e05a9c6978363e68e7e6441bd6ec73c8838fb85
wenghengcong <wenghengcong@gmail.com>
2018-10-22 08:57:27 +0800

    no message

--- bin/hexo-blog-new.sh
+++ bin/hexo-blog-new.sh
@@ -2,4 +2,4 @@
 cd Desktop/MeSync/blog
 #hexo new template file name
 hexo new $1 $2
-mv ~/Desktop/MeSync/blog/source/_posts/$2.md ~/Desktop/MeSync/Next_Blog/$2
+mv ~/Desktop/MeSync/blog/source/_posts/$2.md ~/Desktop/MeSync/Next_Blog/$2.md
