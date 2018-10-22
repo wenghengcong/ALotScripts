c94d9b8241bbf7e6aee3d662a3811abe906e4a12
翁恒丛 <wenghengcong@jd.com>
2018-10-22 17:08:30 +0800

    change my hexo new shell

--- app/bfhexo-blog-new.sh
+++ app/bfhexo-blog-new.sh
@@ -1,5 +1,23 @@
 #cd hexo blog home directory
-cd Desktop/MeSync/blog
+cd ~/Desktop/MeSync/blog/
+echo $PWD
 #hexo new template file name
-hexo new $1 $2
-mv ~/Desktop/MeSync/blog/source/_posts/$2.md ~/Desktop/MeSync/Next_Blog/$2.md
+echo $1
+echo $2
+hexo new $1 "$2"
+
+#如果字符串中有空格，hexo默认会将空格置为-，所以要进行替换
+oriFileName="$2.md"
+#echo "orinigal $oriFileName"
+find=" "
+replace="-"
+result=${oriFileName//$find/$replace}
+#echo "result $result"
+
+#移动文件
+sourceFile="/Users/wenghengcong/Desktop/MeSync/blog/source/_posts/$result"
+desPath="/Users/wenghengcong/Desktop/MeSync/Next_Blog"
+#echo "source file: $sourceFile"
+echo "file now here: $desPath"
+mv -i $sourceFile $desPath
+
