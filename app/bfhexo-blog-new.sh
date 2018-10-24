8696ca6f1f1710c495013f220337e6b91e17ee3c
翁恒丛 <wenghengcong@jd.com>
2018-10-24 19:07:55 +0800

    no message

--- app/bfhexo-blog-new.sh
+++ app/bfhexo-blog-new.sh
@@ -1,23 +1,41 @@
+
+
+# you must pass two parametes,
+# 1. template name:  templates in scaffolds directory，you can custom.
+# 2. md file name:   the blog file name.
+
+#your hexo blog directory
+blogPath=/Users/wenghengcong/Desktop/MeSync/blog
+
+#the path you save pengding blogs
+pendingBlogPath=/Users/wenghengcong/Desktop/MeSync/Next_Blog
+
 #cd hexo blog home directory
-cd ~/Desktop/MeSync/blog/
-echo $PWD
-#hexo new template file name
-echo $1
-echo $2
+cd $blogPath
+
+#echo $PWD
+#hexo new template(I'm just use category as template) filename, templates in scaffolds
+echo "category: $1"
+echo "filename: $2"
+
+#call hexo command to create template md file,templates in scaffolds
+#hexo command will create a md file in blogPath
 hexo new $1 "$2"
 
-#如果字符串中有空格，hexo默认会将空格置为-，所以要进行替换
+#if string contains emptyspace ， it will just replace '-'
 oriFileName="$2.md"
 #echo "orinigal $oriFileName"
 find=" "
 replace="-"
-result=${oriFileName//$find/$replace}
+resultFileName=${oriFileName//$find/$replace}
 #echo "result $result"
+sourceFile="$blogPath/source/_posts/$resultFileName"
 
-#移动文件
-sourceFile="/Users/wenghengcong/Desktop/MeSync/blog/source/_posts/$result"
-desPath="/Users/wenghengcong/Desktop/MeSync/Next_Blog"
-#echo "source file: $sourceFile"
-echo "file now here: $desPath"
-mv -i $sourceFile $desPath
+#move the file form blogPath to pendingBlogPath
+echo "file now here: $pendingBlogPath"
+mv -i $sourceFile $pendingBlogPath
+
+#open the file by Typora,you can replace you used app.
+desPathFile="$pendingBlogPath/$resultFileName"
+open -a "Typora" $desPathFile
 
