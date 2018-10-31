aa775a416f058f807ff1fc7af3e248ac7530d465
wenghengcong <wenghengcong@gmail.com>
2018-11-01 01:11:39 +0800

    no message

--- developer/bfios-usb.sh
+++ developer/bfios-usb.sh
@@ -1,4 +1,12 @@
 # This is a comment too
-echo 'Begin to connecting to iPhone USB port 22' 
-echo 'After conneted, login in 10010 port by ssh' 
-iproxy 10010 22
+echo '---------------------------------------------'
+echo 'SSH login'
+echo 'mac port 10010 connect to iPhone port 22' 
+echo '---------------------------------------------'
+echo 'lldb connect debugserver by mac port 9999 to iPhone port 10086'
+echo '1. In iPhone:  debugserver *:10086 -a App name'
+echo '2. In Mac:     enter lldb'
+echo '3. In Mac:     process connect connect://localhost:9999'
+echo '---------------------------------------------'
+iproxy 10010 22 & \
+iproxy 9999 10086
